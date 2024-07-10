adfee8e3bc99524c43f8bf8dee4282e64b1201fb
Mateus Ferreira <mateus.ieq32@gmail.com>
2024-07-10 17:04:40 -0300

    entidade Participante

---
+++ src/main/resources/db/migration/V2__create-table-participant.sql
@@ -0,0 +1,9 @@
+CREATE TABLE  participants(
+
+    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    is_confirmed BOOLEAN NOT NULL,
+    trip_id UUID,
+    FOREIGN KEY (trip_id) REFERENCES trips(id)
+);
